b7a2282524caa552507e9eab42f9cf383d562c58
Brad Kahl <bkahl1984@gmail.com>
2015-09-30 01:21:12 -0400

    get delete game to work across all games that match

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,17 @@ time = h+':'+m+ampm;
 
 var movies = require('../movies.json');
 
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x1000000000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express', data: movies });
@@ -104,10 +115,11 @@ router.post('/newgame1v1', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
+    var unique_game_id = guid();
 
     var players = [
-        {fullname: req.body.p1name, game: { gameType: 'Singles', position: '*', side: 'yellow', partner: '*', win: (parseInt(req.body.p1score,10) > parseInt(req.body.p2score,10)), loss: (parseInt(req.body.p1score,10) < parseInt(req.body.p2score,10)),  date: date, score: parseInt(req.body.p1score,10), opponentScore: parseInt(req.body.p2score,10), opponentNames: req.body.p2name } },
-        {fullname: req.body.p2name, game: { gameType: 'Singles', position: '*', side: 'black', partner: '*', win: (parseInt(req.body.p2score,10) > parseInt(req.body.p1score,10)), loss: (parseInt(req.body.p2score,10) < parseInt(req.body.p1score,10)),  date: date, score: parseInt(req.body.p2score,10), opponentScore: parseInt(req.body.p1score,10), opponentNames: req.body.p1name } }
+        {fullname: req.body.p1name, game: { id: unique_game_id, gameType: 'Singles', position: '*', side: 'yellow', partner: '*', win: (parseInt(req.body.p1score,10) > parseInt(req.body.p2score,10)), loss: (parseInt(req.body.p1score,10) < parseInt(req.body.p2score,10)),  date: date, score: parseInt(req.body.p1score,10), opponentScore: parseInt(req.body.p2score,10), opponentNames: [req.body.p2name] } },
+        {fullname: req.body.p2name, game: { id: unique_game_id, gameType: 'Singles', position: '*', side: 'black', partner: '*', win: (parseInt(req.body.p2score,10) > parseInt(req.body.p1score,10)), loss: (parseInt(req.body.p2score,10) < parseInt(req.body.p1score,10)),  date: date, score: parseInt(req.body.p2score,10), opponentScore: parseInt(req.body.p1score,10), opponentNames: [req.body.p1name] } }
     ];
 
     // Set our collection
@@ -134,6 +146,7 @@ router.post('/newgame1v1', function(req, res) {
         );   
     }
 
+    console.log("unique game guid",guid());
     res.redirect("/standings");
 });
 
@@ -142,12 +155,13 @@ router.post('/newgame2v2', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
+    var unique_game_id = guid();
 
     var players = [
-        {fullname: req.body.p1name, game: { gameType: 'Doubles', position: 'defense', side: 'yellow', partner: req.body.p2name, win: (parseInt(req.body.team1score,10) > parseInt(req.body.team2score,10)), loss: (parseInt(req.body.team1score,10) < parseInt(req.body.team2score,10)),  date: date, score: parseInt(req.body.team1score,10), opponentScore: parseInt(req.body.team2score,10), opponentNames: [req.body.p3name, req.body.p4name] } },
-        {fullname: req.body.p2name, game: { gameType: 'Doubles', position: 'offense', side: 'yellow', partner: req.body.p1name, win: (parseInt(req.body.team1score,10) > parseInt(req.body.team2score,10)), loss: (parseInt(req.body.team1score,10) < parseInt(req.body.team2score,10)),  date: date, score: parseInt(req.body.team1score,10), opponentScore: parseInt(req.body.team2score,10), opponentNames: [req.body.p3name, req.body.p4name] } },
-        {fullname: req.body.p3name, game: { gameType: 'Doubles', position: 'defense', side: 'black', partner: req.body.p4name, win: (parseInt(req.body.team2score,10) > parseInt(req.body.team1score,10)), loss: (parseInt(req.body.team2score,10) < parseInt(req.body.team1score,10)),  date: date, score: parseInt(req.body.team2score,10), opponentScore: parseInt(req.body.team1score,10), opponentNames: [req.body.p1name, req.body.p2name] } },
-        {fullname: req.body.p4name, game: { gameType: 'Doubles', position: 'offense', side: 'black', partner: req.body.p3name, win: (parseInt(req.body.team2score,10) > parseInt(req.body.team1score,10)), loss: (parseInt(req.body.team2score,10) < parseInt(req.body.team1score,10)),  date: date, score: parseInt(req.body.team2score,10), opponentScore: parseInt(req.body.team1score,10), opponentNames: [req.body.p1name, req.body.p2name] } }
+        {fullname: req.body.p1name, game: { id: unique_game_id, gameType: 'Doubles', position: 'defense', side: 'yellow', partner: req.body.p2name, win: (parseInt(req.body.team1score,10) > parseInt(req.body.team2score,10)), loss: (parseInt(req.body.team1score,10) < parseInt(req.body.team2score,10)),  date: date, score: parseInt(req.body.team1score,10), opponentScore: parseInt(req.body.team2score,10), opponentNames: [req.body.p3name, req.body.p4name] } },
+        {fullname: req.body.p2name, game: { id: unique_game_id, gameType: 'Doubles', position: 'offense', side: 'yellow', partner: req.body.p1name, win: (parseInt(req.body.team1score,10) > parseInt(req.body.team2score,10)), loss: (parseInt(req.body.team1score,10) < parseInt(req.body.team2score,10)),  date: date, score: parseInt(req.body.team1score,10), opponentScore: parseInt(req.body.team2score,10), opponentNames: [req.body.p3name, req.body.p4name] } },
+        {fullname: req.body.p3name, game: { id: unique_game_id, gameType: 'Doubles', position: 'defense', side: 'black', partner: req.body.p4name, win: (parseInt(req.body.team2score,10) > parseInt(req.body.team1score,10)), loss: (parseInt(req.body.team2score,10) < parseInt(req.body.team1score,10)),  date: date, score: parseInt(req.body.team2score,10), opponentScore: parseInt(req.body.team1score,10), opponentNames: [req.body.p1name, req.body.p2name] } },
+        {fullname: req.body.p4name, game: { id: unique_game_id, gameType: 'Doubles', position: 'offense', side: 'black', partner: req.body.p3name, win: (parseInt(req.body.team2score,10) > parseInt(req.body.team1score,10)), loss: (parseInt(req.body.team2score,10) < parseInt(req.body.team1score,10)),  date: date, score: parseInt(req.body.team2score,10), opponentScore: parseInt(req.body.team1score,10), opponentNames: [req.body.p1name, req.body.p2name] } }
     ];
 
     // Set our collection
@@ -255,17 +269,20 @@ router.get('/removegame/:page/:id/:gameindex', function(req, res) {
         updatedUserObj = {};
 
     collection.find({'_id': ObjectId(userID)}).on('success', function (player) { 
-        console.log('player',player[0]);  
+        //console.log('player',player[0]);  
         removedGame = player[0].games.splice(gameindex,1); 
-        console.log('removedGame',removedGame[0]);    
+        //console.log('removedGame',removedGame[0]);    
 
+        removedGameObj.id = removedGame[0].id;
         removedGameObj.gameType = removedGame[0].gameType;
         removedGameObj.position = removedGame[0].position;
         removedGameObj.side = removedGame[0].side;
+        removedGameObj.partner = removedGame[0].partner;
         removedGameObj.win = removedGame[0].win;
         removedGameObj.loss = removedGame[0].loss;
         removedGameObj.score = removedGame[0].score;
         removedGameObj.opponentScore = removedGame[0].opponentScore;
+        removedGameObj.opponentNames = removedGame[0].opponentNames;
 
         updatedUserObj.fullname = player[0].fullname;
         updatedUserObj.fname = player[0].fname;
@@ -285,8 +302,9 @@ router.get('/removegame/:page/:id/:gameindex', function(req, res) {
         updatedUserObj.singleMatches = (removedGameObj.gameType === 'Singles') ? player[0].singleMatches-1 : player[0].singleMatches-0;
         updatedUserObj.games = player[0].games;
 
-        console.log('removedGameObj',removedGameObj);
-        console.log('updatedUserObj',updatedUserObj);
+        //console.log('removedGameObj.id',removedGameObj.id);
+        //console.log('removedGameObj',removedGameObj);
+        //console.log('updatedUserObj',updatedUserObj);
 
         collection.update(
             {'_id': ObjectId(userID)},
@@ -309,7 +327,56 @@ router.get('/removegame/:page/:id/:gameindex', function(req, res) {
                 singleMatches: updatedUserObj.singleMatches,
                 games: updatedUserObj.games
             }
-        ); 
+        );
+
+       //console.log('removedGameObj.opponentNames',removedGameObj.opponentNames);
+       //console.log('removedGameObj.opponentNames.length',removedGameObj.opponentNames.length);
+
+       // Add partner to list of names to update when removing game
+       if(removedGameObj.partner !== '*') removedGameObj.opponentNames.push(removedGameObj.partner)
+
+       for(i=0;i<removedGameObj.opponentNames.length;i++){
+           //console.log('opponentNames.length', removedGameObj.opponentNames.length);
+           //console.log('opponentNames[i]', removedGameObj.opponentNames[i]);
+           //console.log('opponentNames[removedGameObj.opponentNames.length-1]', removedGameObj.opponentNames[removedGameObj.opponentNames.length-1]);
+
+           collection.find({'fullname': removedGameObj.opponentNames[i]}).on('success', function (otherplayer) {
+
+                var otherPlayergameIndex = otherplayer[0].games.map(function(x) {return x.id; }).indexOf(removedGameObj.id);
+                //console.log('otherPlayergameIndex',otherPlayergameIndex);
+                var objectGameFoundToRemove = otherplayer[0].games[otherPlayergameIndex];
+                //console.log('objectGameFoundToRemove',objectGameFoundToRemove);
+
+                var otherplayerRemovedGame = otherplayer[0].games.splice(otherPlayergameIndex,1);
+                //console.log('otherplayerRemovedGame',otherplayerRemovedGame);
+                var otherplayerGamesUpdated = otherplayer[0].games;
+                //console.log('otherplayerGamesUpdated',otherplayerGamesUpdated);
+
+                collection.update(
+                   {'fullname': otherplayer[0].fullname},
+                   {
+                       fullname: otherplayer[0].fullname,
+                       fname: otherplayer[0].fname,
+                       lname: otherplayer[0].lname,
+                       email: otherplayer[0].email,
+                       joined: otherplayer[0].joined,
+
+                       goalsFor: otherplayer[0].goalsFor-otherplayerRemovedGame[0].score,
+                       goalsAgainst: otherplayer[0].goalsAgainst-otherplayerRemovedGame[0].opponentScore,
+                       wins: (otherplayerRemovedGame[0].win) ? otherplayer[0].wins-1 : otherplayer[0].wins-0,
+                       losses: (otherplayerRemovedGame[0].loss) ? otherplayer[0].losses-1 : otherplayer[0].losses-0,
+                       gamesPlayed: otherplayer[0].gamesPlayed-1,
+                       onDefense: (otherplayerRemovedGame[0].position === '*' || otherplayerRemovedGame[0].position === 'offense') ? otherplayer[0].onDefense-0 : otherplayer[0].onDefense-1,
+                       onOffense: (otherplayerRemovedGame[0].position === '*' || otherplayerRemovedGame[0].position === 'defense') ? otherplayer[0].onOffense-0 : otherplayer[0].onOffense-1,
+                       blackSide: (otherplayerRemovedGame[0].side === 'black') ? otherplayer[0].blackSide-1: otherplayer[0].blackSide-0,
+                       yellowSide: (otherplayerRemovedGame[0].side === 'yellow') ? otherplayer[0].yellowSide-1: otherplayer[0].yellowSide-0,
+                       doubleMatches: (otherplayerRemovedGame[0].gameType === 'Doubles') ? otherplayer[0].doubleMatches-1 : otherplayer[0].doubleMatches-0,
+                       singleMatches: (otherplayerRemovedGame[0].gameType === 'Singles') ? otherplayer[0].singleMatches-1 : otherplayer[0].singleMatches-0,
+                       games: otherplayerGamesUpdated
+                   }
+                );
+           });
+       }
     });
 
     res.redirect('/'+page+'/'+userID);
